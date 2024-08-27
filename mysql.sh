a492e2b8a0c60f6a58f06566f7e969883417445c
sivakumarreddym <info@joindevops.com>
2024-08-27 08:53:13 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -43,5 +43,12 @@ VALIDATE $? "Enabled MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
-VALIDATE $? "Setting Up root password"
+mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
