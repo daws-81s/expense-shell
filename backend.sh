edcee540cb9a0f89e104aacc728e003eb0a1ebf1
sivakumarreddym <info@joindevops.com>
2024-08-28 07:31:24 +0530

    shell

---
+++ backend.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "Disable default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enable nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "Install nodejs"
+
+useradd expense
+VALIDATE $? "Creating expense user"
