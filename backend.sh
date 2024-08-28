c14596b3f6959b0258a68287d807f0092095a446
sivakumarreddym <info@joindevops.com>
2024-08-28 07:39:59 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -52,3 +52,13 @@ then
 else
     echo -e "expense user already exists...$Y SKIPPING $N"
 fi
+
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Extracting backend application code"
