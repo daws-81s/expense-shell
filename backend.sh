51f11ca3eca7df7b217a47b9b8c92582cb0e78d4
sivakumarreddym <info@joindevops.com>
2024-08-28 07:33:59 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -34,14 +34,14 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disable default nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enable nodejs:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Install nodejs"
 
-useradd expense
+useradd expense &>>$LOG_FILE
 VALIDATE $? "Creating expense user"
